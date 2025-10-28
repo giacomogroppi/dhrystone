dda866ef2793995542523e5e226b78142200d105
Giacomo Groppi <giamg01@gmail.com>
2025-10-28 09:17:22 +0100

    remove some not used print

--- v2.2/dry.c
+++ v2.2/dry.c
@@ -675,70 +675,13 @@ main (argc, argv) int argc; char *argv[];
     } else Done = true;
   }
 
-  fprintf (stderr, "Final values of the variables used in the benchmark:\n");
-  fprintf (stderr, "\n");
-  fprintf (stderr, "Int_Glob:            %d\n", Int_Glob);
-  fprintf (stderr, "        should be:   %d\n", 5);
-  fprintf (stderr, "Bool_Glob:           %d\n", Bool_Glob);
-  fprintf (stderr, "        should be:   %d\n", 1);
-  fprintf (stderr, "Ch_1_Glob:           %c\n", Ch_1_Glob);
-  fprintf (stderr, "        should be:   %c\n", 'A');
-  fprintf (stderr, "Ch_2_Glob:           %c\n", Ch_2_Glob);
-  fprintf (stderr, "        should be:   %c\n", 'B');
-  fprintf (stderr, "Arr_1_Glob[8]:       %d\n", Arr_1_Glob[8]);
-  fprintf (stderr, "        should be:   %d\n", 7);
-  fprintf (stderr, "Arr_2_Glob[8][7]:    %d\n", Arr_2_Glob[8][7]);
-  fprintf (stderr, "        should be:   Number_Of_Runs + 10\n");
-  fprintf (stderr, "Ptr_Glob->\n");
-  fprintf (stderr, "  Ptr_Comp:          %lu\n", (unsigned long) Ptr_Glob->Ptr_Comp);
-  fprintf (stderr, "        should be:   (implementation-dependent)\n");
-  fprintf (stderr, "  Discr:             %d\n", Ptr_Glob->Discr);
-  fprintf (stderr, "        should be:   %d\n", 0);
-  fprintf (stderr, "  Enum_Comp:         %d\n", Ptr_Glob->variant.var_1.Enum_Comp);
-  fprintf (stderr, "        should be:   %d\n", 2);
-  fprintf (stderr, "  Int_Comp:          %d\n", Ptr_Glob->variant.var_1.Int_Comp);
-  fprintf (stderr, "        should be:   %d\n", 17);
-  fprintf (stderr, "  Str_Comp:          %s\n", Ptr_Glob->variant.var_1.Str_Comp);
-  fprintf (stderr, "        should be:   DHRYSTONE PROGRAM, SOME STRING\n");
-  fprintf (stderr, "Next_Ptr_Glob->\n");
-  fprintf (stderr, "  Ptr_Comp:          %lu\n", (unsigned long) Next_Ptr_Glob->Ptr_Comp);
-  fprintf (stderr, "        should be:   (implementation-dependent), same as above\n");
-  fprintf (stderr, "  Discr:             %d\n", Next_Ptr_Glob->Discr);
-  fprintf (stderr, "        should be:   %d\n", 0);
-  fprintf (stderr, "  Enum_Comp:         %d\n", Next_Ptr_Glob->variant.var_1.Enum_Comp);
-  fprintf (stderr, "        should be:   %d\n", 1);
-  fprintf (stderr, "  Int_Comp:          %d\n", Next_Ptr_Glob->variant.var_1.Int_Comp);
-  fprintf (stderr, "        should be:   %d\n", 18);
-  fprintf (stderr, "  Str_Comp:          %s\n",
-                                Next_Ptr_Glob->variant.var_1.Str_Comp);
-  fprintf (stderr, "        should be:   DHRYSTONE PROGRAM, SOME STRING\n");
-  fprintf (stderr, "Int_1_Loc:           %d\n", Int_1_Loc);
-  fprintf (stderr, "        should be:   %d\n", 5);
-  fprintf (stderr, "Int_2_Loc:           %d\n", Int_2_Loc);
-  fprintf (stderr, "        should be:   %d\n", 13);
-  fprintf (stderr, "Int_3_Loc:           %d\n", Int_3_Loc);
-  fprintf (stderr, "        should be:   %d\n", 7);
-  fprintf (stderr, "Enum_Loc:            %d\n", Enum_Loc);
-  fprintf (stderr, "        should be:   %d\n", 1);
-  fprintf (stderr, "Str_1_Loc:           %s\n", Str_1_Loc);
-  fprintf (stderr, "        should be:   DHRYSTONE PROGRAM, 1'ST STRING\n");
-  fprintf (stderr, "Str_2_Loc:           %s\n", Str_2_Loc);
-  fprintf (stderr, "        should be:   DHRYSTONE PROGRAM, 2'ND STRING\n");
-  fprintf (stderr, "\n");
-
-
-    Microseconds = (float) User_Time * Mic_secs_Per_Second 
-                        / ((float) HZ * ((float) Number_Of_Runs));
     Dhrystones_Per_Second = ((float) HZ * (float) Number_Of_Runs)
                         / (float) User_Time;
 
-    printf ("Microseconds for one run through Dhrystone: ");
-    printf ("%10.1f \n", Microseconds);
     printf("---- start test ----\n");
     printf ("[dhrystone] drysone_p_sec_%d %lu\n", CONFIG_RAMSPEED_SIZE_MEMORY, (unsigned long) Dhrystones_Per_Second);
     printf("---- stop test ----\n");
     reboot_test_finished();
-    printf ("\n");
   
 }
 
