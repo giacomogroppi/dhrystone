b50c4d8cb5929e3cba2836563f7786be8854f7f9
Giacomo Groppi <giamg01@gmail.com>
2025-10-28 09:26:46 +0100

    add multiple runs

--- v2.2/dry.c
+++ v2.2/dry.c
@@ -602,85 +602,92 @@ main (argc, argv) int argc; char *argv[];
   printf ("Using %s, HZ=%d\n", CLOCK_TYPE, HZ);
   printf ("\n");
 
-  Done = false;
-  while (!Done) {
+   printf("---- start test ----\n");
 
-    printf ("Trying %d runs through Dhrystone:\n", Number_Of_Runs);
+  for (unsigned iii = 0; iii < 1000; iii ++) {
+    Done = false;
+      while (!Done) {
 
-    /***************/
-    /* Start timer */
-    /***************/
+        printf ("Trying %d runs through Dhrystone:\n", Number_Of_Runs);
 
-    Start_Timer();
+        /***************/
+        /* Start timer */
+        /***************/
 
-    for (Run_Index = 1; Run_Index <= Number_Of_Runs; ++Run_Index)
-    {
+        Start_Timer();
 
-      Proc_5();
-      Proc_4();
-	/* Ch_1_Glob == 'A', Ch_2_Glob == 'B', Bool_Glob == true */
-      Int_1_Loc = 2;
-      Int_2_Loc = 3;
-      strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
-      Enum_Loc = Ident_2;
-      Bool_Glob = ! Func_2 (Str_1_Loc, Str_2_Loc);
-	/* Bool_Glob == 1 */
-      while (Int_1_Loc < Int_2_Loc)  /* loop body executed once */
-      {
-	Int_3_Loc = 5 * Int_1_Loc - Int_2_Loc;
-	  /* Int_3_Loc == 7 */
-	Proc_7 (Int_1_Loc, Int_2_Loc, &Int_3_Loc);
-	  /* Int_3_Loc == 7 */
-	Int_1_Loc += 1;
-      } /* while */
-	/* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-      Proc_8 (Arr_1_Glob, Arr_2_Glob, Int_1_Loc, Int_3_Loc);
-	/* Int_Glob == 5 */
-      Proc_1 (Ptr_Glob);
-      for (Ch_Index = 'A'; Ch_Index <= Ch_2_Glob; ++Ch_Index)
-			       /* loop body executed twice */
-      {
-	if (Enum_Loc == Func_1 (Ch_Index, 'C'))
-	    /* then, not executed */
-	  {
-	  Proc_6 (Ident_1, &Enum_Loc);
-	  strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 3'RD STRING");
-	  Int_2_Loc = Run_Index;
-	  Int_Glob = Run_Index;
-	  }
+        for (Run_Index = 1; Run_Index <= Number_Of_Runs; ++Run_Index)
+        {
+
+          Proc_5();
+          Proc_4();
+      /* Ch_1_Glob == 'A', Ch_2_Glob == 'B', Bool_Glob == true */
+          Int_1_Loc = 2;
+          Int_2_Loc = 3;
+          strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
+          Enum_Loc = Ident_2;
+          Bool_Glob = ! Func_2 (Str_1_Loc, Str_2_Loc);
+      /* Bool_Glob == 1 */
+          while (Int_1_Loc < Int_2_Loc)  /* loop body executed once */
+          {
+      Int_3_Loc = 5 * Int_1_Loc - Int_2_Loc;
+        /* Int_3_Loc == 7 */
+      Proc_7 (Int_1_Loc, Int_2_Loc, &Int_3_Loc);
+        /* Int_3_Loc == 7 */
+      Int_1_Loc += 1;
+          } /* while */
+      /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
+          Proc_8 (Arr_1_Glob, Arr_2_Glob, Int_1_Loc, Int_3_Loc);
+      /* Int_Glob == 5 */
+          Proc_1 (Ptr_Glob);
+          for (Ch_Index = 'A'; Ch_Index <= Ch_2_Glob; ++Ch_Index)
+                /* loop body executed twice */
+          {
+      if (Enum_Loc == Func_1 (Ch_Index, 'C'))
+          /* then, not executed */
+        {
+        Proc_6 (Ident_1, &Enum_Loc);
+        strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 3'RD STRING");
+        Int_2_Loc = Run_Index;
+        Int_Glob = Run_Index;
+        }
+          }
+      /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
+          Int_2_Loc = Int_2_Loc * Int_1_Loc;
+          Int_1_Loc = Int_2_Loc / Int_3_Loc;
+          Int_2_Loc = 7 * (Int_2_Loc - Int_3_Loc) - Int_1_Loc;
+      /* Int_1_Loc == 1, Int_2_Loc == 13, Int_3_Loc == 7 */
+          Proc_2 (&Int_1_Loc);
+      /* Int_1_Loc == 5 */
+
+        } /* loop "for Run_Index" */
+
+        /**************/
+        /* Stop timer */
+        /**************/
+
+        Stop_Timer();
+
+        User_Time = End_Time - Begin_Time;
+
+        if (User_Time < Too_Small_Time)
+        {
+          printf ("Measured time too small to obtain meaningful results\n");
+          Number_Of_Runs = Number_Of_Runs * 10;
+          printf ("\n");
+        } else Done = true;
       }
-	/* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-      Int_2_Loc = Int_2_Loc * Int_1_Loc;
-      Int_1_Loc = Int_2_Loc / Int_3_Loc;
-      Int_2_Loc = 7 * (Int_2_Loc - Int_3_Loc) - Int_1_Loc;
-	/* Int_1_Loc == 1, Int_2_Loc == 13, Int_3_Loc == 7 */
-      Proc_2 (&Int_1_Loc);
-	/* Int_1_Loc == 5 */
 
-    } /* loop "for Run_Index" */
-
-    /**************/
-    /* Stop timer */
-    /**************/
-
-    Stop_Timer();
-
-    User_Time = End_Time - Begin_Time;
-
-    if (User_Time < Too_Small_Time)
-    {
-      printf ("Measured time too small to obtain meaningful results\n");
-      Number_Of_Runs = Number_Of_Runs * 10;
-      printf ("\n");
-    } else Done = true;
-  }
-
-    Dhrystones_Per_Second = ((float) HZ * (float) Number_Of_Runs)
+       Dhrystones_Per_Second = ((float) HZ * (float) Number_Of_Runs)
                         / (float) User_Time;
 
-    printf("---- start test ----\n");
+   
     printf ("[dhrystone] drysone_p_sec_%d %lu\n", CONFIG_RAMSPEED_SIZE_MEMORY, (unsigned long) Dhrystones_Per_Second);
-    printf("---- stop test ----\n");
+    
+  }
+  
+
+   printf("---- stop test ----\n");
     reboot_test_finished();
   
 }
